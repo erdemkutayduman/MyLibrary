e78f38954d0964513e18a282c3c6282d30b0ab14
Erdem Duman <77546927+erdemmduman@users.noreply.github.com>
2021-01-24 17:43:13 +0300

    Add files via upload

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriYoneticisi musteriManager = new MusteriYoneticisi();
+            musteriManager.Ekle(11122, "Erdem Duman", "Erkek", 27);
+            musteriManager.Ekle(11133, "Sude Sesigüzel", "Kadın", 28);
+            musteriManager.Ekle(11144, "Ümit Toprak", "Erkek", 54);
+            musteriManager.Ekle(11155, "Hasan Can", "Erkek", 35);
+            musteriManager.Ekle(11166, "Tülin Öztürk", "Kadın", 42);
+
+            musteriManager.Sil(11155);
+            musteriManager.Listele();
+            
+        }
+    }
+}
