2af0270c4f2924b1450e37c496dcc5b28f299341
Erdem Kutay Duman <77546927+erdemkutayduman@users.noreply.github.com>
2021-04-22 00:22:03 +0300

    #JavaCamp

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,56 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// camelCase
+		// CTRL + Space
+		
+		String internetSubeButonu = "Ýnternet Þubesi \n";
+		double dolarDun = 8.18;
+		double dolarBugun = 8.18;
+		int vade = 36;
+		boolean degisim = false;
+		
+		System.out.println(internetSubeButonu);
+		
+		if(dolarBugun<dolarDun) {
+			System.out.println("Dolar Düþtü!");
+		}
+		
+		
+		else if (dolarBugun>dolarDun) {
+			System.out.println("Dolar Yükseldi!");
+		}
+			
+		
+		else{
+			System.out.println("Dolar Eþittir!");
+		}
+		
+		String[] krediler = 
+			{
+					"Hýzlý Kredi \n",
+					"Mutlu Emekli Kredisi \n",
+					"Konut Kredisi \n",
+					"Msb Kredisi \n",
+					"Meb Kredisi \n",
+					"Kültür Bakanlýðý Kredisi \n"
+			};
+		
+		for (String kredi : krediler) 
+		{
+			System.out.print(kredi);			
+		}
+		
+		System.out.print("\n");
+		
+		for (int i = 0; i < krediler.length; i++) 
+		{			
+			System.out.print(krediler[i]);
+		}
+		
+	}
+
+}
